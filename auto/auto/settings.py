c5ee9355d5ada0fee8e802b85e54dbb6c3bd6dcf
ChestnutCMD <13can133@gmail.com>
2025-11-05 04:18:05 +0500

    new commit

--- auto/auto/settings.py
+++ auto/auto/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS')]
+os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
 
 INSTALLED_APPS = [
     'admin_interface',
