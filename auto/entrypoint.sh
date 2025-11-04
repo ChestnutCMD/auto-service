395afa44b088d6369de9e405a573c3b50d536a1b
ChestnutCMD <13can133@gmail.com>
2025-11-05 03:55:01 +0500

    new commit

--- auto/entrypoint.sh
+++ auto/entrypoint.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 set -e
-
-python manage.py migrate --check
-status=$?
-if [[ $status != 0 ]]; then
-    python manage.py migrate --noinput
-fi
-
+python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-
 exec "$@"
