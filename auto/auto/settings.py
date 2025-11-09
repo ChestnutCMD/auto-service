8d3cbc38dcdc604753c57011a13a45c958f1076d
ChestnutCMD <13can133@gmail.com>
2025-11-09 16:30:51 +0500

    update

--- auto/auto/settings.py
+++ auto/auto/settings.py
@@ -115,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
 LANGUAGE_CODE = 'ru-ru'
-TIME_ZONE = 'Europe/Moscow'
+TIME_ZONE = 'Asia/Yekaterinburg'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
