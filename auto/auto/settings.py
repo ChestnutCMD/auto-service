8b1db3760e9f033bc11c3924366475807853e8fa
ChestnutCMD <13can133@gmail.com>
2026-04-17 17:36:00 +0500

    fix email notification

--- auto/auto/settings.py
+++ auto/auto/settings.py
@@ -141,11 +141,11 @@ TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 # Константа с email получателя
-RECIPIENT_EMAIL = os.getenv('EMAIL_HOST_USER')
+NOTIFICATION_EMAIL = os.getenv('EMAIL_HOST_USER')
 
 # Настройки SMTP
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.mail.ru'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
